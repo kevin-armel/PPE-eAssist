8a94c61df062cbef2795f94ac3ae73ac407836fe
Kevin-Armel <kevin-armel.sonkeng@edu.ece.fr>
2019-03-19 11:08:39 +0100

    Optimisation des interface IHM pour une utilisation plus ergonomique
    Optimisation des interface IHM pour une utilisation plus ergonomique

---
+++ Assets/Scripts/LoadAnotherSceneOnClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAnotherSceneOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        //Application.LoadLevel("Menu 3D");
+        //Scene current = ;
+        if (SceneManager.Equals(SceneManager.GetActiveScene(), SceneManager.GetSceneAt(0))) //.sceneLoaded)
+        {
+            SceneManager.UnloadSceneAsync(1);
+            SceneManager.UnloadSceneAsync(2);
+        }else if (SceneManager.Equals(SceneManager.GetActiveScene(), SceneManager.GetSceneAt(1)))
+        {
+            SceneManager.UnloadSceneAsync(0);
+            SceneManager.UnloadSceneAsync(2);
+        }else if(SceneManager.Equals(SceneManager.GetActiveScene(), SceneManager.GetSceneAt(2)))
+        {
+            SceneManager.UnloadSceneAsync(1);
+            SceneManager.UnloadSceneAsync(0);
+        }
+    }
+}
